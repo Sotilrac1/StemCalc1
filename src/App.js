03017d7a5ae86bd1f240e32ca25b70f6ea3ab8f2
CarlosF <18237370+Sotilrac1@users.noreply.github.com>
2023-08-15 20:48:34 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+import "./styles.css";
+import "./styles/NavBar.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+function NavBar() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        {" "}
+        Stem Calc{" "}
+      </a>
+      <ul>
+        <li>
+          <a href="/calc" className="menu-item">
+            Calculator
+          </a>
+        </li>
+        <li>
+          <a href="/about" className="menu-item">
+            About
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
