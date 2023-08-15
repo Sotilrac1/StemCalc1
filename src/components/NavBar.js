312d256648df223cc3f8707279873a536cfec84b
Carlos Flynn <18237370+Sotilrac1@users.noreply.github.com>
2023-08-15 12:44:32 +0000

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,29 @@
 export default function NavBar() {
+  
   return (
     <nav className="nav">
       <a href="/" className="site-title">
         Stem Calc
       </a>
-      <ul className="active">
-        <li>
-          <a href="/calculator" className="menu-item">
-            Calculator
-          </a>
-        </li>
-        <li>
-          <a href="/about" className="menu-item">
-            About
-          </a>
-        </li>
+      <ul>
+          <CustomLink href="/calculator">Calculator</CustomLink>
+          <CustomLink href="/about">About</CustomLink>
       </ul>
     </nav>
   );
+}
+
+
+function CustomLink({href,children,...props}){
+  const path = window.location.pathname
+  return(
+    <li className={path === href ? "active":""}> 
+    <a href={href} {...props}>
+      {children}
+    </a>
+  </li>
+  )
+
 }
 
 //export default NavBar;
